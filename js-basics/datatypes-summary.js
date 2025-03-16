faa53ed43e8bbf1a69a53c96b0596b42ce21e60a
kshitij3103 <153099642+kshitij3103@users.noreply.github.com>
2025-03-16 07:42:04 +0000

    datatype summary

---
+++ js-basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+/*primitive
+
+7 types : String ,Number , Boolean ,Null , undefined , symbol,bigint
+
+called by value
+*/
+
+/* Reference (Non Primitve)
+
+
+
+array , objects , functions 
+
+
+*/
