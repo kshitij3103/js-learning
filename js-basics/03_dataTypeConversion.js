13c5963a7f361f6fdccbe7bf8fe9e3f92532ab41
kshitij3103 <153099642+kshitij3103@users.noreply.github.com>
2025-03-16 07:13:47 +0000

    data type conversion

--- js-basics/03_dataTypeConversion.js
+++ js-basics/03_dataTypeConversion.js
@@ -1,8 +1,8 @@
-let score="33abc"
-console.log(typeof score);
-let val_num=Number(score);
-console.log(typeof val_num);
-console.log(val_num)
+// let score="33abc"
+// console.log(typeof score);
+// let val_num=Number(score);
+// console.log(typeof val_num);
+// console.log(val_num)
 
 // "33" -> 33
 // "33abc" ->Nan
@@ -10,4 +10,12 @@ console.log(val_num)
 
 // 1-> true 0->false
 // "" -> false  "something"-> true
+
+// *******operations*********
+let value =3;
+let neg_val=-value
+console.log(neg_val);
+ 
+
+
 
