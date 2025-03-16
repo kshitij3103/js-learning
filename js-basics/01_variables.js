b09c81e496159c9ba70c0511879157e77da97b27
kshitij3103 <153099642+kshitij3103@users.noreply.github.com>
2025-03-16 06:41:24 +0000

    commit 2

---
+++ js-basics/01_variables.js
@@ -0,0 +1,12 @@
+const accnt_id=144553;
+let accnt_emmail="absc@hh.com";
+var accnt_password="12345";
+accnt_city="Jaipur";
+accnt_email="hfhfhff";
+accnt_city="delhi";
+// accnt_id=2;
+ console.log(accnt_id);
+ console.table([accnt_id,accnt_password,accnt_city,accnt_email]);
+ /*
+ not to use var bea=cause of blocked scope
+ */
---
+++ js-basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";
+// alert(3+3); 
+let name="kshitij"
+let age=18;
+let is_logged_in=false;
+
+//number -> 2^53
+// bigint 
+//string
+//boolean
+//null -> standalone value
+//undefined -> 
+//symbol
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
