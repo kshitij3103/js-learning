a1136d91bba26014d34dab923c47b8d54c521acc
kshitij3103 <153099642+kshitij3103@users.noreply.github.com>
2025-03-23 05:48:27 +0000

    arrays 1

---
+++ js-basics/02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+//array
+
+const my_arr =[0,1,2,3,4,5];
+const my_arr2 = new Array(3,4,54,6); 
+// my_arr2.push(88);
+// my_arr2.pop();
+// my_arr.unshift(9); // insert at array beginning and shift
+
+
+// my_arr.shift(); // removes first element
+
+
+ // includes ()  .indexOf()
+//  console.log(my_arr);
+ 
+// const new_arr= my_arr.join(); // makes string
+// console.log(typeof new_arr);
+
+
+//slice splice
+
+const arr=my_arr.slice(0,3); // slices part of array //start and end idx
+console.log(arr);
+console.log(my_arr);
+
+const arr2=my_arr.splice(0,3); // removes elements of array //start idx and count
+console.log(arr2); 
+console.log(my_arr);
+
+
+
+ 
+
+
+
+
+
+
+
+
